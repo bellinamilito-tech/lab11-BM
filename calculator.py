0a17694f69acab0f49ba0aaa4f90df9da58434e6
bellinamilito-tech <bellina.milito@gmail.com>
2025-11-12 12:11:22 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,7 +5,39 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+# https://github.com/YOUR_USERNAME/lab11-P1
+# Partner 1: Bellina
+# Partner 2: Bellina
 
+import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid base or argument for logarithm")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
