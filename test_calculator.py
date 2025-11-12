0d627b658644f7bc364c537b7a2c9ebb06d5a63a
bellinamilito-tech <bellina.milito@gmail.com>
2025-11-12 12:21:22 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,17 @@
 # https://github.com/bellinamillito-tech/lab11-BM
+# Partner 1: Bellina
+# Partner 2: Bellina
 
 import unittest
-from calculator import *
-
-class TestCalculator(unittest.TestCase):
-
-import unittest
-import math
 import calculator
 
 class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculator.add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(10, 4), 6)
 
     def test_multiply(self):
         self.assertEqual(calculator.mul(3, 4), 12)
@@ -17,32 +19,26 @@ class TestCalculator(unittest.TestCase):
     def test_divide(self):
         self.assertEqual(calculator.div(2, 10), 5)
 
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            calculator.div(0, 5)
+
+    def test_logarithm(self):
+        self.assertAlmostEqual(calculator.logarithm(10, 100), 2.0)
+
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            calculator.log(1, -5)
+            calculator.logarithm(1, -5)
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            calculator.logarithm(1, 10)
 
     def test_hypotenuse(self):
         self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5.0)
 
     def test_sqrt(self):
         self.assertAlmostEqual(calculator.square_root(25), 5.0)
-
-    def test_add(self):
-        self.assertEqual(calculator.add(2, 3), 5)
-
-    def test_subtract(self):
-        self.assertEqual(calculator.sub(10, 4), 6)
-
-    def test_divide_by_zero(self):
-        with self.assertRaises(ZeroDivisionError):
-            calculator.div(0, 5)
-
-    def test_logarithm(self):
-        self.assertAlmostEqual(calculator.log(10, 100), 2.0)
-
-    def test_log_invalid_base(self):
-        with self.assertRaises(ValueError):
-            calculator.log(1, 10)
 
 # Do not touch this
 if __name__ == "__main__":
